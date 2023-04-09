408deff3a20a7fa65b60754aeea4ea1dd01943af
prottyaRoy5011 <prottya.roy@orbitax.com>
2023-04-09 12:58:23 +0600

    exceptions handled

---
+++ WebApplication1/Models/UserToken.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserToken
+    {
+        public string _id { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ WebApplication1/Services/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+        public string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user._id)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject =  new ClaimsIdentity(claims), 
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            Console.WriteLine(token);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
